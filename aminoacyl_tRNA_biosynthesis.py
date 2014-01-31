4f9ba8d7edd2e3b3ccea10eb7d6eff3e692973c5
CoreyLeo <Corey.Leonardi@bc.edu>
2014-01-31 12:51:46 -0500

    charge tRNA-Val reaction

--- aminoacyl_tRNA_biosynthesis.py
+++ aminoacyl_tRNA_biosynthesis.py
@@ -11,5 +11,26 @@ c = cytoplasm.localizer
 Metabolite.default_location = cytoplasm
 
 
+tRNA_val = RNA("tRNA_val")
+L_valine = Metabolite("L_valine")
 atp = Metabolite("atp")
 adp = Metabolite("adp")
+#need to create tRNA...write RNA class? or use metabolite?
+
+chargeValRNA = Reaction(name = 'chargeValRNA'
+                        reactants= tRNA_val + L_valine)
+
+SP_0568 = Gene("SP_0568")
+chargeValRNA_GA = GeneAssociation(chargeValRNA, SP_0568)
+#gene association: valyl-tRNA synthetase
+
+#REACTION TEMPLATE
+#NAME = Reaction(name="NAME",
+#                       reactants=REACT1 + REACT2,
+#                       products=PROD1 + PROD2,
+#                       pairs=[(pair1, pair1), (pair2, pair2)],
+#                       minors=[minor1, minor2])
+
+#GENE ASSOCIATION TEMPLATE
+#GENE = Gene("GENE")
+#REACTIONNAME_GA = GeneAssociation(REACTIONNAME, GENE)
