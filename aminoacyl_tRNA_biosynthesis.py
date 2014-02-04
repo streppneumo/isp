68bd3eae6b82f4f39e75acefd25ff689f39adc1f
CoreyLeo <Corey.Leonardi@bc.edu>
2014-02-03 21:50:10 -0500

    moved things around, added list of similar AAs
    need to write function for “simple” tRNA charging so that we can use
    one simple function for all these AAs and condense code

--- aminoacyl_tRNA_biosynthesis.py
+++ aminoacyl_tRNA_biosynthesis.py
@@ -2,6 +2,16 @@ __author__ = 'Corey'
 
 from CellScribe import *
 
+#REACTION TEMPLATE
+#NAME = Reaction(name="NAME",
+#                       reactants=REACT1 + REACT2,
+#                       products=PROD1 + PROD2,
+#                       pairs=[(pair1, pair1), (pair2, pair2)],
+#                       minors=[minor1, minor2])
+
+#GENE ASSOCIATION TEMPLATE
+#GENE = Gene("GENE")
+#REACTIONNAME_GA = GeneAssociation(REACTIONNAME, GENE)
 
 extracellular = Location("Extracellular", 'e')
 e = extracellular.localizer
@@ -10,11 +20,16 @@ cytoplasm = Location("Cytoplasm", 'c')
 c = cytoplasm.localizer
 Metabolite.default_location = cytoplasm
 
+atp = Metabolite("atp")
+amp = Metabolite("adp")
+
+#CHARGE tRNA-Val REACTION. reaction is identical for following amino acids (gene associations below):
+#Leucine, Isoleucine, Arginine, Proline, Histidine, Tyrosine, Tryptophan, Alanine, Threonine, Serine, Methionine
+#SP_0254.... 1659..... 2078.... 0264...... 2121...... 2100...... 2229.... 1383..... 1639...... 0411..... 0788
+
 tRNA_val = RNA("tRNA_val")
 L_valine = Metabolite("L_valine")
 L_valyl_tRNA = RNA("L_valyl_tRNA")
-atp = Metabolite("atp")
-amp = Metabolite("adp")
 #need to create tRNA...write RNA class? or use metabolite?
 
 chargeValRNA = Reaction(name='chargeValRNA',
@@ -27,13 +42,3 @@ SP_0568 = Gene("SP_0568")
 chargeValRNA_GA = GeneAssociation(chargeValRNA, SP_0568)
 #gene association: valyl-tRNA synthetase
 
-#REACTION TEMPLATE
-#NAME = Reaction(name="NAME",
-#                       reactants=REACT1 + REACT2,
-#                       products=PROD1 + PROD2,
-#                       pairs=[(pair1, pair1), (pair2, pair2)],
-#                       minors=[minor1, minor2])
-
-#GENE ASSOCIATION TEMPLATE
-#GENE = Gene("GENE")
-#REACTIONNAME_GA = GeneAssociation(REACTIONNAME, GENE)
