b3d3b34587271df7a0ab10a018b306c01cc0f6d0
CoreyLeo <Corey.Leonardi@bc.edu>
2014-02-05 23:59:21 -0500

    added kegg IDs for all metabolites

--- aminoacyl_tRNA_biosynthesis.py
+++ aminoacyl_tRNA_biosynthesis.py
@@ -9,17 +9,17 @@ cytoplasm = Location("Cytoplasm", 'c')
 c = cytoplasm.localizer
 Metabolite.default_location = cytoplasm
 
-atp = Metabolite("atp")
-amp = Metabolite("amp")
+atp = Metabolite("atp", kegg="C00002")
+amp = Metabolite("amp", kegg="C00020")
 
 #generic charge simple tRNA
 
 
-def charge_trna_simple(aaname, trnaname, chargedrna_name, *associated_genes):
+def charge_trna_simple(aaname, aakegg, trnaname, chargedrna_name, chargedkegg, *associated_genes):
 
-    trna = RNA(trnaname)
-    amino_acid = Metabolite(aaname)
-    charged_trna = Metabolite(chargedrna_name)
+    trna = RNA(trnaname)  # do tRNAs have kegg IDs?
+    amino_acid = Metabolite(aaname, kegg=aakegg)
+    charged_trna = Metabolite(chargedrna_name, kegg=chargedkegg)
 
     charge_trna = Reaction(name='charge' + aaname,
                            reactants=trna + amino_acid + atp,
@@ -29,33 +29,31 @@ def charge_trna_simple(aaname, trnaname, chargedrna_name, *associated_genes):
 
     GeneAssociation(charge_trna, associated_genes)
 
-charge_trna_simple("L-valine", "tRNA_val", "L_valyl_tRNA", Gene('SP_0568'))
-charge_trna_simple("L_leucine", "tRNA_leu", "L_leucyl_tRNA", Gene('SP_0254'))
-charge_trna_simple("L_isoleucine", "tRNA_ile", "L_isoleucyl_tRNA", Gene('SP_1659'))
-charge_trna_simple("L-lysine", "tRNA_lys", "L_lysyl_tRNA", Gene('SP_0713'))
-charge_trna_simple("L_arginine", "tRNA_arg", "L_arginyl_tRNA", Gene('SP_2078'))
-charge_trna_simple("L_proline", "tRNA_pro", "L_prolyl_tRNA", Gene('SP_0264'))
-charge_trna_simple("L_histidine", "tRNA_his", "L_histidyl_tRNA", Gene('SP_2121'))
-charge_trna_simple("L_phenylalanine", "tRNA_phe", "L_phenylalanyl_tRNA", Gene('SP_0581') & Gene('SP_0579'))
-charge_trna_simple("L_tyrosine", "tRNA_tyr", "L_tyrosyl_tRNA", Gene('SP_2100'))
-charge_trna_simple("L_tryptophan", "tRNA_trp", "L_tryptophanyl_tRNA", Gene('SP_2229'))
-charge_trna_simple("L_alanine", "tRNA_ala", "L_alanyl_tRNA", Gene('SP_1383'))
-charge_trna_simple("L_glycine", "tRNA_gly", "L_glycyl_tRNA", Gene('SP_1474') & Gene('SP_1475'))
-charge_trna_simple("L_threonine", "tRNA_thr", "L_threonyl_tRNA", Gene('1631'))
-charge_trna_simple("L_serine", "tRNA_ser", "L_seryl_tRNA", Gene('SP_0411'))
-charge_trna_simple("L_methionine", "tRNA_met", "L_methionyl_tRNA", Gene('SP_0788'))
+charge_trna_simple("L-valine", "C00183", "tRNA_val", "L_valyl_tRNA", "C02554", Gene('SP_0568'))
+charge_trna_simple("L_leucine", "C00123", "tRNA_leu", "L_leucyl_tRNA", "C02047", Gene('SP_0254'))
+charge_trna_simple("L_isoleucine", "C00407", "tRNA_ile", "L_isoleucyl_tRNA", "C03127", Gene('SP_1659'))
+charge_trna_simple("L_lysine", "C00047", "tRNA_lys", "L_lysyl_tRNA", "C01931", Gene('SP_0713'))
+charge_trna_simple("L_arginine", "C00062", "tRNA_arg", "L_arginyl_tRNA", "C02163", Gene('SP_2078'))
+charge_trna_simple("L_proline", "C00148", "tRNA_pro", "L_prolyl_tRNA", "C02702", Gene('SP_0264'))
+charge_trna_simple("L_histidine", "C00135", "tRNA_his", "L_histidyl_tRNA", "C02988", Gene('SP_2121'))
+charge_trna_simple("L_phenylalanine", "C00079", "tRNA_phe", "L_phenylalanyl_tRNA", "C03511", Gene('SP_0581') & Gene('SP_0579'))
+charge_trna_simple("L_tyrosine", "C00082", "tRNA_tyr", "L_tyrosyl_tRNA", "C02839", Gene('SP_2100'))
+charge_trna_simple("L_tryptophan", "C00078", "tRNA_trp", "L_tryptophanyl_tRNA", "C03512", Gene('SP_2229'))
+charge_trna_simple("L_alanine", "C00041", "tRNA_ala", "L_alanyl_tRNA", "C00886", Gene('SP_1383'))
+charge_trna_simple("L_glycine", "C00037", "tRNA_gly", "L_glycyl_tRNA", "C02412", Gene('SP_1474') & Gene('SP_1475'))
+charge_trna_simple("L_threonine", "C00188", "tRNA_thr", "L_threonyl_tRNA", "C02992", Gene('1631'))
+charge_trna_simple("L_serine", "C00065", "tRNA_ser", "L_seryl_tRNA", "C02553", Gene('SP_0411'))
+charge_trna_simple("L_methionine", "C00073", "tRNA_met", "L_methionyl_tRNA", "C02430", Gene('SP_0788'))
 
 # L_glutamyl_tRNA from glutamate and tRNA_gln. get to L_glutaminyl_tRNA through SP_0436, SP_0437, SP_0438
-charge_trna_simple("L_glutamate", "tRNA_gln", "L_glutamyl_tRNA", "SP_2069")
-
-# L_seryl_tRNA using
+charge_trna_simple("L_glutamate", "C00025", "tRNA_gln", "L_glutamyl_tRNA", "C06112", Gene('SP_2069'))
 
 # make N_formyl_methionyl_tRNA
-ten_formyl_THF = Metabolite("ten_formyl_THF")
-L_methionyl_tRNA = Metabolite("L_methionyl_tRNA")
-THF = Metabolite("THF")
-N_formylmethionyl_tRNA = Metabolite("N_formylmethionyl_tRNA")
-h2o = Metabolite("h2o")
+ten_formyl_THF = Metabolite("ten_formyl_THF", kegg="C00234")
+L_methionyl_tRNA = Metabolite("L_methionyl_tRNA", kegg="C02430")
+THF = Metabolite("THF", kegg="C00101")
+N_formylmethionyl_tRNA = Metabolite("N_formylmethionyl_tRNA", kegg="C03294")
+h2o = Metabolite("h2o", kegg="C00001")
 
 met_tRNA_formyltransferase = Reaction(name='met_tRNA_formyltransferase',
                                       reactants=h2o + ten_formyl_THF + L_methionyl_tRNA,
