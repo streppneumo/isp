61d59f0aa5610bf5ca98d5ce9b0ba46cabb6355d
CoreyLeo <Corey.Leonardi@bc.edu>
2014-02-06 00:19:23 -0500

    added cysteine, asparagine rxns

--- aminoacyl_tRNA_biosynthesis.py
+++ aminoacyl_tRNA_biosynthesis.py
@@ -29,7 +29,7 @@ def charge_trna_simple(aaname, aakegg, trnaname, chargedrna_name, chargedkegg, *
 
     GeneAssociation(charge_trna, associated_genes)
 
-charge_trna_simple("L-valine", "C00183", "tRNA_val", "L_valyl_tRNA", "C02554", Gene('SP_0568'))
+charge_trna_simple("L_valine", "C00183", "tRNA_val", "L_valyl_tRNA", "C02554", Gene('SP_0568'))
 charge_trna_simple("L_leucine", "C00123", "tRNA_leu", "L_leucyl_tRNA", "C02047", Gene('SP_0254'))
 charge_trna_simple("L_isoleucine", "C00407", "tRNA_ile", "L_isoleucyl_tRNA", "C03127", Gene('SP_1659'))
 charge_trna_simple("L_lysine", "C00047", "tRNA_lys", "L_lysyl_tRNA", "C01931", Gene('SP_0713'))
@@ -45,9 +45,13 @@ charge_trna_simple("L_glycine", "C00037", "tRNA_gly", "L_glycyl_tRNA", "C02412",
 charge_trna_simple("L_threonine", "C00188", "tRNA_thr", "L_threonyl_tRNA", "C02992", Gene('1631'))
 charge_trna_simple("L_serine", "C00065", "tRNA_ser", "L_seryl_tRNA", "C02553", Gene('SP_0411'))
 charge_trna_simple("L_methionine", "C00073", "tRNA_met", "L_methionyl_tRNA", "C02430", Gene('SP_0788'))
+charge_trna_simple("L_cysteine", "C00097", "tRNA_cys", "L_cysteinyl_tRNA", "C03125", Gene('SP_0591'))
+charge_trna_simple("L_asparagine", "C00152", "tRNA_asn", "L_asparaginyl_tRNA", "C03402", Gene("SP_1542"))
 
 # L_glutamyl_tRNA from glutamate and tRNA_gln. get to L_glutaminyl_tRNA through SP_0436, SP_0437, SP_0438
 charge_trna_simple("L_glutamate", "C00025", "tRNA_gln", "L_glutamyl_tRNA", "C06112", Gene('SP_2069'))
+
+# L_asparginyl_tRNA from aspartate and tRNA_asn. 
 
 # make N_formyl_methionyl_tRNA
 ten_formyl_THF = Metabolite("ten_formyl_THF", kegg="C00234")
