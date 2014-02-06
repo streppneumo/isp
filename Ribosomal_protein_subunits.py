199656c43f0d266ddb2547c740e3bff11d3df49a
laruer14 <laruer@bc.edu>
2014-02-06 11:39:42 -0500

    Complexes underway

--- Ribosomal_protein_subunits.py
+++ Ribosomal_protein_subunits.py
@@ -15,18 +15,21 @@ def proteinSubunits(subunits, builtPsubunit):
             products=builtPsubunit)
 
 S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
-S16_complex = Complex("S16_complex")
+S9_complex = Complex("S9_complex")
+S13_complex = Complex("S13_complex")
+S19_complex = Complex("S19_complex")
+S12_complex = Complex("S12_complex")
+S5_complex = Complex("S5_complex")
+S21_complex = Complex("S21_complex")
+S6_S18_complex = Complex("S6_S18_complex")
+S10_complex = Complex("S10_complex")
+S14_complex = Complex("S14_complex")
+S3_complex = Complex("S3_complex")
+S2_complex = Complex("S2_complex")
 
 
 
-proteinSubunits(Gene("SP_0838") & Gene("SP_0085"), Gene("SP_0775")) #S16
+proteinSubunits(Gene("SP_0838") & Gene("SP_0085") & Gene("SP_0775"), ) #S16
 proteinSubunits(Gene("SP_0272"), Gene("SP_0295")) #S9
 proteinSubunits(Gene("SP_0272"), Gene("SP_0234"))#S13
 proteinSubunits(Gene("SP_0272"), Gene("SP_0213"))#S19
@@ -34,7 +37,7 @@ proteinSubunits(Gene("SP_0218") & Gene("SP_0775") & Gene("SP_0224") & Gene("SP_0
 proteinSubunits(Gene("SP_0775") & Gene("SP_0224"), Gene("SP_0227"))#S5
 proteinSubunits(Gene("SP_1541") & Gene("SP_1539"), Gene("SP_0235"))#S11
 proteinSubunits(Gene("SP_1541") & Gene("SP_1539") & Gene("SP_0227"), Gene("SP_1414"))#S21
-proteinSubunits(Gene("SP_1626"), Gene("SP_1541") & Gene("SP_1539"))#S6/S18 complex (heterodimers)
+proteinSubunits(Gene("SP_1626"), Gene("SP_1541") & Gene("SP_1539"))#S6_S18 complex (heterodimers)
 proteinSubunits(Gene("SP_0295"),  Gene("SP_0208"))#S10
 proteinSubunits(Gene("SP_0208") & Gene("SP_0295") & Gene("SP_0213"), Gene("SP_0222"))#S14
 proteinSubunits(Gene("SP_0227") & Gene("SP_0208"), Gene("SP_0215"))#S3
@@ -42,6 +45,8 @@ proteinSubunits(Gene("SP_0215"), Gene("SP_2215"))#S2
 #proteinSubunits(subunits, buildPsubunit)
 
 
+def proteinSubunitsL(subunitsL, builtPsubunitL):
+    
 
 
 
