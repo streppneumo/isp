cfd646ae9cc58d61ad47f3c80c5963ce1b76cac2
laruer14 <laruer@bc.edu>
2014-02-09 19:57:08 -0500

    removed comments

--- Ribosomal_protein_subunits.py
+++ Ribosomal_protein_subunits.py
@@ -65,27 +65,3 @@ S2_complex = Complex("S2_complex")
     
 
 
-
-
-
-
-            #pairs=[(glucose, glucose6phosphate), (atp, adp)],
-            #minors=[atp, adp])
-
-
-##glucose_transport1 = Reaction(name="glucose_transport1",
-##                              reactants=e(glucose) + atp,
-##                              products=glucose + adp)
-##
-##SP_1000 = Gene("SP_1000")
-##SP_1001 = Gene("SP_1001")
-##SP_1002 = Gene("SP_1002")
-##
-##glucose_transport1_GA = GeneAssociation(glucose_transport1, SP_1000 & (SP_1001 | SP_1002))
-##
-##if __name__ == "__main__":
-##    print glucokinase
-##    #print glucokinase_GA
-##
-##    #print glucose_transport1
-##    #print glucose_transport1_GA
