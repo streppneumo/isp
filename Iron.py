40b0d3345a38c6bd1efd221c10ec9a81b4083372
doanw <doanw@bc.edu>
2014-02-18 11:25:39 -0500

    new complex

--- Iron.py
+++ Iron.py
@@ -14,7 +14,7 @@ phosphate = Metabolite("phosphate")
 H20 = Metabolite("H20",kegg="C00001")
 ferrichrome = Metabolite("ferrichrome", kegg="C06228")
 
-#iron vs ferrichrome?
+#iron vs ferrichrome? reaction states that it transfers ferrichrome
 
 ferrichrome = Reaction(name="ferrichrome_ABC",
                       reactants=e(ferrichrome) + H20 + atp,
@@ -26,6 +26,7 @@ SP_1032 = Gene("SP_1032")
 SP_1033 = Gene("SP_1033")
 SP_1034 = Gene("SP_1034")
 SP_1035 = Gene("SP_1035")
-ferrichrome_GA = GeneAssociation(ferrichrome, SP_1032 & SP_1033 & SP_1034 & SP_1035)
+ferrichrome_GA = GeneAssociation(ferrichrome, SP_1032 & SP_1033 & SP_1034 & SP_1035 or
+                                              SP_1869 & SP_1870 & SP_1871 & SP_1872)
 
-# also (SP_1869 SP_1870 SP_1871 SP_1872) another cluster
+#Pit 2 is first line & Pit 1 is second line
