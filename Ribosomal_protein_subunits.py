edf68bbb824ad664d764d2a7fe777d5f3ee55641
laruer14 <laruer@bc.edu>
2014-02-27 11:17:59 -0500

    Completed 5S rRNA interaction with 23S rRNA

--- Ribosomal_protein_subunits.py
+++ Ribosomal_protein_subunits.py
@@ -92,12 +92,7 @@ proteinSubunitsL(Gene("SP_0229") & Gene("SP_0237") & Gene("SP_0441"), L28_comple
 proteinSubunitsL(Gene("SP_0229") & Gene("SP_1107"), L27_complex)
 proteinSubunitsL(Gene("SP_0225") & Gene("SP_1355") & Gene("SP_0630") & Gene("SP_0216"), L16_complex)
 #proteinSubunitsL(Gene("SP_0229") & Gene("SP_2215"), L25_complex) NOT IN STREP. BUT IT IS IN E. COLI
-proteinSubunitsL(Gene("SP_0221") & Gene("SP_0229") & Gene("SP_0226") & ((L2 & L3) | (L2 & L4) | (L3 & L4)) & Gene("SP_rrnaA5S"), 5S_rrna)
+proteinSubunitsL(Gene("SP_0221") & Gene("SP_0229") & Gene("SP_0226") & ((Gene("SP_0212") & Gene("SP_0209")) | (Gene("SP_0212") & Gene("SP_0210")) | (Gene("SP_0209") & Gene("SP_0210"))) & Gene("SP_rrnaA5S"), 5S_rrna)
 proteinSubunitsL(Gene("SP_0220") & Gene("SP_0961") & Gene("SP_0210") & Gene("SP_0211") & Gene("SP_0209")\
                  & Gene("SP_2204") & Gene("SP_0631") & Gene("SP_0212"), & Gene("SP_rrnaA23S"), 23S_rrna)
-
-
-#FOR LATER: add to 5S binding that any TWO of the proteins L2, L3, and L4 are necessary for
-#the binding of 5S to 23S RNA. Do as another fxn call!! Need all L5 & L15 & L18...then
-# & ((L2 & L3) | (L2 & L4) | (L3 & L4) ...etc
 
