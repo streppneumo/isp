92b832dacf0ab2f030f2cf31278107433d146c72
laruer14 <laruer@bc.edu>
2014-04-01 10:02:06 -0400

    Helicase encoded

--- model/expression/replication_machinery.py
+++ model/expression/replication_machinery.py
@@ -1,7 +1,7 @@
 #Replication fork machinery
 from CellScribe import *
 from genes import SP_0993, SP_0274, SP_0895, SP_0865, SP_0936, SP_765, SP_0002, \
-     SP_
+     SP_2203, SP_
 
 
 def replicationmachinery(subunits, builtPsubunit):
@@ -9,15 +9,15 @@ def replicationmachinery(subunits, builtPsubunit):
             reactants= subunits,
             products=builtPsubunit)
 
-DNA_pol_iii = Complex("DNA_polIII")
-helicase = Enzyme("S9_complex")
+DNA_pol_iii = Complex("DNA_pol_iii")
+helicase = Enzyme("helicase")
 primase = Enzyme("S13_complex")
 DNA_pol_i = Enzyme("S19_complex")
 DNA_ligase = Complex("S12_complex")
 
 
 replicationmachinery(SP_0993 & SP_0274 & SP_0895 & SP_0865, SP_0936, SP_765, SP_0002), DNA_pol_iii)
-proteinSubunits(Gene("SP_0272") & Gene("SP_0295"), helicase) #S9
+replicationmachinery(SP_2203, helicase)
 proteinSubunits(Gene("SP_0272") & Gene("SP_0234"), S13_complex)#S13
 proteinSubunits(Gene("SP_0272") & Gene("SP_0213"), S19_complex)#S19
 proteinSubunits(Gene("SP_0218") & Gene("SP_0775") & Gene("SP_0224") & Gene("SP_0227") & Gene("SP_0271"), S12_complex)#S12
