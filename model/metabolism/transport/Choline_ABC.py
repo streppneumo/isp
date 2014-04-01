ebe2cb06404dc8252790fd9eb59fd06b6cc468d5
doanw <doanw@bc.edu>
2014-04-01 09:02:15 -0400

    cleaned up

--- model/metabolism/transport/Choline_ABC.py
+++ model/metabolism/transport/Choline_ABC.py
@@ -4,11 +4,6 @@ from metabolites import *
 from genes import SP_1860, SP_1861
 
 
-choline_rxn = Reaction(name="choline_rxn",
-                    reactants=e(choline) + atp,
-                    products=choline + adp + phosphate)
-
-GeneAssociation(choline_rxn, SP_1860 & SP_1861)
 
 choline = Metabolite("choline", kegg="C00114")
 choline_rxn = Reaction(name="choline_rxn",
--- model/metabolism/transport/Iron.py
+++ model/metabolism/transport/Iron.py
@@ -6,18 +6,19 @@ from genes import SP_1032, SP_1033, SP_1034, SP_1035, SP_1869, SP_1870, SP_1871,
 
 
 iron = Metabolite("iron", kegg="C14819")
-
 iron_rxn = Reaction(name="iron_rxn",
                            reactants=e(iron) + H20 + atp,
                            products=iron + adp + phosphate,
-                           pairs=[(atp, adp)],
-                           minors=[atp, adp])
+                           Reversible=true,
+                           pairs=[(e(iron), iron)])
 
 
 GeneAssociation(iron_rxn, SP_1032 & SP_1033 & SP_1034 & SP_1035 or SP_1869 & SP_1870 & SP_1871 & SP_1872
 or SP_0241 & SP_0242 & SP_0243)
 
+
 #Pit 2 is first & Pit 1 is second & Pit is last
 #SP_0241 isn't in genome spreadsheet
 
 #regulation
+
