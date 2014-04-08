f7618e469cdaec384d19727f5da6e624b5aaec09
doanw <doanw@bc.edu>
2014-04-08 17:50:07 -0400

    notes

--- model/metabolism/transport/Zinc_ABC.py
+++ model/metabolism/transport/Zinc_ABC.py
@@ -1,13 +1,12 @@
 from CellScribe import *
-from compartments import e, c
-from metabolites import *
-from genes import SP_2169, SP_2170, SP_2171
+from model.compartments import e
+from model.metabolites import *
+from model.genes import SP_2169, SP_2170, SP_2171, SP_1858, SP_1857
 
 
 zinc_rxn = Reaction(name="zinc_rxn",
                     reactants=e(zinc) + H20 + atp,
                     products=zinc + adp + phosphate,
-                    reversible=True,
                     pairs=[(e(zinc), zinc)])
 
 GeneAssociation(zinc_rxn, SP_2169 & SP_2170 & SP_2171)
@@ -17,6 +16,6 @@ GeneAssociation(zinc_rxn, SP_2169 & SP_2170 & SP_2171)
 SczA = Gene("SP_1858")
 czcD = Gene("SP_1857")
 
-# If(SczA, ~czcD)
-# If (Zn2+, ~SczA)
+If(SczA, ~czcD)
+If(Zn, ~SczA)
 
