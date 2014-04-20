38f00e7c2c18735a8dae78bc29de7f0331126b13
Amanda Whittaker <whittaam@bc.edu>
2014-04-19 20:45:13 -0400

    Small edits

--- model/metabolism/pathways/Galactose Metabolism.py
+++ model/metabolism/pathways/Galactose Metabolism.py
@@ -3,10 +3,13 @@ __author__ = 'amanda'
 #FOR GITHUB CLICK REPOSITORY FROM TOP BAR THEN 'PULL' THEN COMMIT!
 
 from CellScribe.main import *
-from model.compartments import e, c
+from model.compartments import *
 from model.metabolites import *
-from model.genes import *
-from model.operons import *
+
+from model.genes import SP_2092, SP_0358, SP_1607, SP_1828, SP_1867, SP_1829, SP_1852, SP_1853, SP_0066, SP_0060
+from model.genes import SP_0648, SP_0476, SP_1185, SP_1186, SP_0668, SP_1898, SP_1898, SP_0477, SP_1184, SP_1192
+from model.genes import SP_1193, SP_0645, SP_0646, SP_0647, SP_1197, SP_1198, SP_1190, SP_0896, SP_1191, SP_0321
+from model.genes import SP_0323, SP_0324, SP_0325, SP_1724, SP_1795, SP_1724
 
 
 
