cc43628feab8430dc3ad0f002d456adb9b797b3c
doanw <doanw@bc.edu>
2014-04-28 13:20:11 -0400

    zinc

--- model/metabolism/transport/Zinc_Manganese_ABC.py
+++ model/metabolism/transport/Zinc_Manganese_ABC.py
@@ -2,6 +2,7 @@ from CellScribe import *
 from model.compartments import e
 from model.metabolites import *
 from model.genes import SP_2169, SP_2170, SP_2171, SP_1648, SP_1649, SP_1650, SP_1858, SP_1857, SP_1552
+from model.operons import psaBCA
 
 zinc = Metabolite("zinc", kegg="C00038")
 zinc_rxn = Reaction(name="zinc_rxn",
@@ -10,28 +11,21 @@ zinc_rxn = Reaction(name="zinc_rxn",
                     pairs=[(e(zinc), zinc)])
 GeneAssociation(zinc_rxn, SP_2169 & SP_2170 & SP_2171)
 
-SczA = Gene("SP_1858")
-czcD = Gene("SP_1857")
-
-If(SczA, ~czcD)
-If(zinc, ~SczA)
-
-# If(SP_1858, ~SP_1857)
-# If(zinc, ~SP_1858)
-
 manganese = Metabolite("manganese", kegg="C00034")
 manganese_rxn = Reaction(name="manganese_rxn",
                        reactants=e(manganese) + atp,
                        products=manganese + adp + phosphate,
                        pairs=[(e(manganese), manganese)])
 
+SczA = Gene("SP_1858")
+czcD = Gene("SP_1857")
+
+If(SczA, ~czcD)
+If(zinc, ~SczA)
+
 GeneAssociation(manganese_rxn, SP_1648 & SP_1649 & SP_1650)
 
 PsaR = Gene("SP_1638")
-psaBCA = Gene("SP_1650")
-psaBCA = Gene("SP_1649")
-psaBCA = Gene("SP_1648")
-
 
 If(PsaR & (zinc | manganese), ~psaBCA)
 
