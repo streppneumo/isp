80e01c646bcf6fc4ef40f16d84c98c39c989e728
doanw <doanw@bc.edu>
2014-04-30 20:00:02 -0400

    fixed reactions

--- model/metabolism/transport/Sugar_ABC.py
+++ model/metabolism/transport/Sugar_ABC.py
@@ -4,6 +4,7 @@ from model.metabolites import *
 from model.genes import SP_1386, SP_1387, SP_1388, SP_1389, SP_1386, SP_1387, SP_1388, SP_1389, SP_1580
 
 spermidine = Metabolite("spermidine", kegg="C00315")
+putrescine = Metabolite("putrescine", kegg="C00134")
 spermidine_rxn = Reaction(name="spermidine_rxn",
                           reactants=e(spermidine) + h2o + atp,
                           products=spermidine + adp + phosphate,
@@ -13,40 +14,36 @@ spermidine_rxn = Reaction(name="spermidine_rxn",
 GeneAssociation(spermidine_rxn, SP_1386 & SP_1387 & SP_1388 & SP_1389)
 
 
-putrescine = Metabolite("putriscine", kegg="C00134")
 putrescine_rxn = Reaction(name="putrescine_rxn",
                           reactants=e(putrescine) + h2o + atp,
-                          products=glucose + adp + phosphate)
-
-#kegg pathway states that glucose is the product.... but it should be putrescine right?
+                          products=putrescine + adp + phosphate)
 
 GeneAssociation(putrescine_rxn, SP_1386 & SP_1387 & SP_1388 & SP_1389)
 
-
-
+cellobiose = Metabolite("cellobiose", kegg="C00185")
 cellobiose_rxn = Reaction(name="cellobiose_rxn",
-                          reactants=e(cellobiose) + H20 + atp,
+                          reactants=e(cellobiose) + h2o + atp,
                           products=cellobiose + adp + phosphate)
 
+chitobiose = Metabolite("chitobiose", kegg="C01674")
 chitobiose_rxn = Reaction(name="chitobiose_rxn",
-                          reactants=e(chitobiose) + H20 + adp,
+                          reactants=e(chitobiose) + h2o + adp,
                           products=chitobiose + adp + phosphate)
 
 GeneAssociation(cellobiose_rxn or chitobiose_rxn, SP_1580)
 
 
-
+mannitol = Metabolite("manitol", kegg="C00392")
 mannitol_rxn = Reaction(name="mannitol_rxn",
-                        reactants=e(mannitol) + H20 + atp,
+                        reactants=e(mannitol) + h2o + atp,
                         products=mannitol + adp + phosphate)
 
 GeneAssociation(mannitol_rxn, SP_1580)
 
-sucrose = Reaction(name="sucrose",
-                      reactants=e(sucrose) + H20 + atp,
+sucrose = Metabolite("sucrose", kegg="C00089")
+sucrose_rxn = Reaction(name="sucrose_rxn",
+                      reactants=e(sucrose) + h2o + atp,
                       products=sucrose + adp + phosphate)
-
-SP_1580 = Gene("SP_1580")
-sucrose_GA = GeneAssociation(sucrose, SP_1580)
+GeneAssociation(sucrose_rxn, SP_1580)
 
 #sucrose, cellobiose, and chitobiose share the same 1 gene... how to approach this?
